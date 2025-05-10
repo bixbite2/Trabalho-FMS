8e408198c1e949fecc14e2417cb33958526c55ff
Bix <lasdo2@outlook.com>
2025-05-10 15:25:11 -0300

    começo de threads

--- main.c
+++ main.c
@@ -7,16 +7,28 @@
 #include "usage_monitor.h"
 #include "usage_interface.h"
 
+#define NUMBER_OF_THREADS 10
+
 const int N = 300;
-pthread_t thread;
 
 int main(int argc, char *argv[])
 {
+    pthread_t threads[NUMBER_OF_THREADS];
+
     handle_error_not_enough_args(argc);
     get_usage_params();
     int pid = fork( );
     char program[N];
     strncpy(program, argv[1], N-1);
+    int status, i;
+    for(i=0; i < NUMBER_OF_THREADS; i++) {
+        printf("Metodo Main. Criando thread %d\n", i);
+            status = pthread_create(&threads[i], NULL, usage_monitor, (void *)i);
+        if (status != 0) {
+            printf("Oops. pthread create returned error code %d\n", status);
+            exit(-1);
+        }
+    }
 
     if (pid < 0) {
         printf("Erro ao criar o processo!\n");
--- usage_monitor.h
+++ usage_monitor.h
@@ -1,1 +1,13 @@
 #include <pthread.h>
+#include <stdio.h>
+
+// Thread function prototype
+void *usage_monitor(void *tid);
+
+// Example thread function
+void *usage_monitor(void *tid) {
+  // Code that will be executed by the new thread
+  printf("Thread is running\n");
+  // Return a value from the thread (optional)
+  pthread_exit(NULL); // Terminates the thread
+}
